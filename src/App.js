a108b5d6d78c760ed5faeb998cfe24c7e9c282a4
Alex Krentsel <akrentsel@berkeley.edu>
2022-04-18 21:49:48 -0700

    created kishucounter page

--- src/App.js
+++ src/App.js
@@ -8,24 +8,6 @@ import KishuCounter from './KishuCounter.js';
 
 function App() {
   return KishuCounter();
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload. Ahilex Krentsel!
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
